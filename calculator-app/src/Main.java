68f964d83194d996fa055e92911092cb73359826
Dmitrii Podlesnykh <podlesnykh.dmitrii@gmail.com>
2020-03-11 19:22:02 +0300

    Add Calculator Project

---
+++ calculator-app/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int a = 232324324;
+        int b = 1;
+        int c;
+        c = Calculator.sum(a, b);
+        System.out.println(c);
+        System.out.println(Calculator.minus(a, b));
+    }
+
+}
