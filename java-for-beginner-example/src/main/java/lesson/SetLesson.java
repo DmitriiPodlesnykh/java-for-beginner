bf1451cb8c6d467e67c54cdc7786ca0522598865
Dmitrii Podlesnykh <podlesnykh.dmitrii@gmail.com>
2020-05-27 12:26:20 +0300

    HashSet lesson fix import

--- java-for-beginner-example/src/main/java/lesson/SetLesson.java
+++ java-for-beginner-example/src/main/java/lesson/SetLesson.java
@@ -1,15 +1,8 @@
 package lesson;
 
 import animal.Dog;
-import animal.RightLeg;
-import animal.Sheep;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class SetLesson implements Lesson, CheckBoolean
 {
